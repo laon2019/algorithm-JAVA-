4d8927593119383238bb5518a9647890460fac28
laon2019 <laon5987@nate.com>
2022-12-02 22:15:06 +0900

    algolithm

---
+++ algorithm/src/Test2751/Test2751.java
@@ -0,0 +1,23 @@
+package Test2751;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test2751 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int[] array = new int[N];
+		for(int i=0;i<N;i++) {
+			array[i] = in.nextInt();
+		}
+		Arrays.sort(array);
+		
+		for(int j : array) {
+			System.out.println(j);
+		}
+	}
+
+}
