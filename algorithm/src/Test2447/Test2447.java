2d35d45fed7bdc05c98debb4ec6f01911788e605
laon2019 <laon5987@nate.com>
2022-12-17 23:59:04 +0900

    algolithm

---
+++ algorithm/src/Test2447/Test2447.java
@@ -0,0 +1,15 @@
+package Test2447;
+
+import java.util.Scanner;
+
+public class Test2447 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				System.out.println("*");
+			}
+		}
+	}
+}
