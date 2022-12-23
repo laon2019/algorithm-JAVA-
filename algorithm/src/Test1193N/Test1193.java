e83df5d49555b8c52c36e0c7ce02b243c0657f52
laon2019 <laon5987@nate.com>
2022-12-24 00:01:23 +0900

    algorithm

--- algorithm/src/Test1193N/Test1193.java
+++ algorithm/src/Test1193N/Test1193.java
@@ -1,7 +1,17 @@
 package Test1193N;
 
+import java.util.Scanner;
+
 public class Test1193 {
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i =1;i<=n;i++) {
+			if(n%3==2) {
+				System.out.println(" ");
+			}else {
+				System.out.println("*");
+			}
+		}
 	}
 }
